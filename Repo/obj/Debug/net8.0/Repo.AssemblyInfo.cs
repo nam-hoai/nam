0ca43257cc8c31e5277effdcd6b0648f0047732f
unknown <namdhhe153102@fpt.edu..vn>
2025-06-15 02:31:41 +0700

    update1.2

--- Repo/obj/Debug/net8.0/Repo.AssemblyInfo.cs
+++ Repo/obj/Debug/net8.0/Repo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10c361f27444ca52db07d561a8c324b5f22ef9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a51465bbd6cc33456d87b127b8243bdd8dc805")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
